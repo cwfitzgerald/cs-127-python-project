7f77530bd07dd4c187a405ebe25aed5c2925e750
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2017-11-30 19:54:42 -0500

    Working changes

--- datasets/add_dataset.py
+++ datasets/add_dataset.py
@@ -1,29 +1,65 @@
 import sqlite3
 import csv
 import sys
-import pickle
+import json
+import os
+import itertools
+import operator
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
-        print("Usage: {} <input_csv> <id_col>")
+    if (sys.argv[1] in ["--delete", "-d"]):
+        delete = True
+    else:
+        delete = False
+
+    if (len(sys.argv) < 3):
+        print("Usage: {} [<input_csv>]*")
         exit(1)
 
-    filename = sys.argv[1]
-    id_col = int(sys.argv[2])
+    settings = json.load(open("index.json"))
 
-    connect = sqlite3.connect("datasets.csv")
+    csv.field_size_limit(2**31 - 1)
+
+    filenames = list(map(os.path.abspath, sys.argv[2 if delete else 1:]))
+    filenames_short = list(map(os.path.basename, filenames))
+
+    in_json = [x in settings for x in filenames_short]
+
+    for f, j in zip(filenames, in_json):
+        if not j:
+            print("{} isn't in .json".format(f))
+
+    filenames = list(itertools.compress(filenames, in_json))
+    filenames_short = list(itertools.compress(filenames_short, in_json))
+    id_cols = [settings[f]["document_column"] for f in filenames_short]
+
+    connect = sqlite3.connect("datasets.sql")
 
     c = connect.cursor()
 
     c.execute('''CREATE TABLE IF NOT EXISTS data (filename text, id text, contents text)''')
-    c.execute('''DELETE FROM data WHERE filename = :name''', {"name": filename})
 
-    csv_reader = csv.reader(open(filename, encoding="utf8", errors="ignore"))
-    csv_reader.__next__()  # Skip header
+    for filename, shortname, col in zip(filenames, filenames_short, id_cols):
+        print("Adding file {}:".format(filename))
 
-    for row in csv_reader:
-        id_val = row[id_col]
-        c.execute('''INSERT INTO data (filename, id, contents) VALUES (:filename, :id, :contents)''',
-                  {"filename": filename, "id": id_val, "contents": pickle.dump(row)})
+        if (delete):
+            c.execute('''DELETE FROM data WHERE filename = :name''', {"name": shortname})
 
-    c.commit()
+        csv_reader = csv.reader(open(filename, encoding="utf8", errors="ignore"))
+        csv_reader.__next__()  # Skip header
+
+        i = 0
+
+        for row in csv_reader:
+            id_val = row[col]
+
+            sys.stdout.write("\033[K\rWriting row #{}: {}".format(i, id_val))
+
+            c.execute('''INSERT OR IGNORE INTO data (filename, id, contents) VALUES (:filename, :id, :contents)''',
+                      {"filename": shortname, "id": id_val, "contents": json.dumps(row)})
+
+            i += 1
+
+        print()
+
+    connect.commit()
