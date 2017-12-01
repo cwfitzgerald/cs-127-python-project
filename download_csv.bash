365d2df2b0897548beae2855a3819f567f66b9f1
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2017-11-30 22:27:28 -0500

    Fix wget downloads

--- download_csv.bash
+++ download_csv.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget www.static.connorwfitzgerald.com/csv_cache/{offenders.tar.gz,gutenberg.tar.gz} -P datasets
+wget www.static.connorwfitzgerald.com/csv_cache/{offenders.tar.gz,gutenberg.tar.gz} -P datasets -nc
 
 for i in $( find datasets -type f -name '*.tar.gz' ); do
 	tar -xf $i -C .
-	echo $i "->" ${i%%.*}.tar.gz
+	echo $i "->" ${i%%.*}.csv
 done
 
