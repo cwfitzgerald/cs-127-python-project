0841665e5b4d96289644d658b692f1075541d729
Anton Goretsky <anton.goretsky@gmail.com>
2017-12-19 01:44:46 -0500

    add server

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app.secret_key = "hello"
 # Temporary list of data list filenames.
 
 # Human Readable list of CSV Datasets (Hardcoded)
-csv_list = [("Bus Breakdowns", 'bus_breakdowns.csv'), ("Offenders", 'offenders.csv'), ("Gutenberg (ALL)", 'gutenberg.csv'), ('Schools', 'schools.csv')]
+csv_list = [("Bus Breakdowns", 'bus_breakdowns.csv'), ("Offenders", 'offenders.csv'), ('Schools', 'schools.csv')]
 
 @app.route("/")
 def main_page():
